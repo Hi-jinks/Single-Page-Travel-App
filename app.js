3a5f94e57733acd2ab37e9d506b336346f4a50d0
Hi-jinks <catherinedoo272@gmail.com>
2023-01-23 22:19:36 +0000

    Now send get request to list cities & sites upon click. Fix.

--- app.js
+++ app.js
@@ -4,16 +4,34 @@ const app = express();
 const fs = require('fs');
 
 const cities = require('./cities.json');
+const sites = require('./sites.json');
 
 app.use(express.json());
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'client')));
 
-/* To list cities */
+/* To list cities 
 app.get('/cities/:continent', function (req, resp) {
-    const cityKeys = Object.keys(cities);
-    resp.send(cityKeys);
+    const cityText = cities[0]
+    resp.send(cityText);
 });
+
+app.get('/cities', function(req, resp) {
+    const cityKeys = Object.keys(cities);
+    resp.send(cityKeys); 
+}) */
+
+/* List cities for a continent */
+app.get('/cities', function(req, resp){
+    const citiesText = Object.keys(cities);
+    resp.send(citiesText);
+})
+
+/*To list sites for a specific city */
+app.get('/sites', function(req, resp){
+    const sitesText = Object.keys(sites);
+    resp.send(sitesText);
+})
 
 module.exports = app;
 
---
+++ client/listCities.js
@@ -0,0 +1,43 @@
+const endpoint = 'http://127.0.0.1:8090/';
+
+/*
+async function listCities (continent) {
+    const cityResponse = await fetch(`http://127.0.0.1:8090/cities/${continent}`);
+    const cityNamesText = await cityResponse.text();
+    const cityNames = JSON.parse(cityNamesText);
+    const cityListElt = document.getElementById('list_african_cities');
+    let list = '';
+    for (const cityName of cityNames) {
+        list += `<li class='city_list_item'>${cityName}</li>`;
+    }
+    cityListElt.innerHTML = list;
+    const listItems = document.querySelectorAll('.city_list_item');
+    for (const listItem of listItems) {
+        listItem.addEventListener('click', (event) => loadCity(event.target.textContent));
+    } 
+}
+
+let africa_button = document.getElementById('african_cities');
+
+africa_button.addEventListener('click', async function listCity (event){
+    const cityResponse = await fetch(`http://127.0.0.1:8090/cities`);
+    let body = await response.text();
+    document.getElementById('list_african_cities').innerHTML = body;
+})
+
+document.addEventListener('DOMContentLoaded', listCities);
+document.addEventListener('DOMContentLoaded', listCity);
+
+*/
+
+
+let african_cities = document.getElementById('african_cities');
+
+function addButton() {
+african_cities.addEventListener('click', async function listCities (event){ /*event*/
+    const cityResponse = await fetch(`http://127.0.0.1:8090/cities`);
+    let body = await cityResponse.text();
+    document.getElementById("list_african_cities").innerHTML = body;
+})}
+
+document.addEventListener('DOMContentLoaded', addButton);
